885e12d4ab54bd51ea0b03cd795e4512d32cb84e
Randolph Saludo <randolphsaludo22@gmail.com>
2025-04-29 18:26:20 +0800

    Fixed the form style

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -1,73 +1,73 @@
 <form wire:submit="register" class="gap-6 flex flex-col">
-    <header class="text-center">
-        <h3 class="text-4xl font-bold text-gray-800 dark:text-neutral-200">Create your Account</h3>
-        <p class="mt-2 text-gray-500 dark:text-neutral-400">This is where your posting journey begins.</p>
-    </header>
     <div class="flex flex-col gap-4">
         <div class="flex flex-col md:flex-row gap-4">
             <div class="full">
-                <x-input-label for="firstname" :value="__('First Name')" />
+                <x-input-label for="firstname" :value="__('First Name*')" />
                 <x-text-input wire:model="firstname" id="firstname" type="text" name="firstname"
                     placeholder="Enter your Firstname" class="block w-full mt-1" required autofocus
                     autocomplete="firstname" />
                 <x-input-error :messages="$errors->get('firstname')" class="mt-2" />
             </div>
             <div class="full">
-                <x-input-label for="lastname" :value="__('Last Name')" />
+                <x-input-label for="lastname" :value="__('Last Name*')" />
                 <x-text-input wire:model="lastname" id="lastname" type="text" name="lastname"
                     placeholder="Enter your Lastname" class="block w-full mt-1" required autofocus
                     autocomplete="lastname" />
                 <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
             </div>
+            <div class="full">
+                <x-input-label for="middlename" :value="__('Middle Name (Optional)')" />
+                <x-text-input wire:model="middlename" id="middlename" type="text" name="middlename"
+                    placeholder="Enter your Middlename" class="block w-full mt-1" autofocus
+                    autocomplete="middlename" />
+                <x-input-error :messages="$errors->get('middlename')" class="mt-2" />
+            </div>
         </div>
 
         <div class="flex flex-col md:flex-row gap-4">
             <div class="w-full">
-                <x-input-label for="email" :value="__('Email')" />
+                <x-input-label for="email" :value="__('Email*')" />
                 <x-text-input wire:model="email" id="email" type="email" name="email"
                     placeholder="Enter your Email" class="block w-full mt-1" required autocomplete="email" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
             <div class="w-full">
-                <x-input-label for="phone" :value="__('Phone')" />
+                <x-input-label for="phone" :value="__('Phone (Optional)')" />
                 <x-text-input wire:model="phone" id="phone" type="text" name="phone"
-                    placeholder="ex. 0912 345 6789" class="block w-full mt-1" required autocomplete="tel" />
+                    placeholder="ex. 0912 345 6789" class="block w-full mt-1" autocomplete="tel" />
                 <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+            </div>
+
+            <div class="w-full">
+                <x-input-label for="birthday" :value="__('Birthday*')" />
+                <x-date-input wire:model="birthday" id="birthday" name="birthday" class="block w-full mt-1" required
+                    autocomplete="bday" />
+                <x-input-error :messages="$errors->get('birthday')" class="mt-2" />
             </div>
         </div>
 
         <div>
-            <x-input-label for="birthday" :value="__('Birthday')" />
-            <x-date-input wire:model="birthday" id="birthday" name="birthday" class="block w-full mt-1" required
-                autocomplete="bday" />
-            <x-input-error :messages="$errors->get('birthday')" class="mt-2" />
-        </div>
-
-        <div>
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Password*')" />
             <x-password-input wire:model="password" id="password" name="password"
                 placeholder="Create a Strong Password" class="block w-full mt-1" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <div>
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirm Password*')" />
             <x-password-input wire:model="password_confirmation" id="password_confirmation" name="password_confirmation"
                 placeholder="Confirm your Password" class="block w-full mt-1" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
     </div>
-
-    <div>
+    <div class="w-auto gap-4 flex flex-col justify-center items-start">
+        <p class="text-sm text-gray-600 dark:text-neutral-400">By clicking Create Account, you agree to our Terms,
+            Privacy Policy and Cookies Policy. <br> You may receieve SMS Notifications from us and can opt out any time.
+        </p>
         <x-primary-button class="w-full">
             {{ __('Create Account') }}
         </x-primary-button>
-        <p class="text-center mt-4 text-sm text-gray-600 dark:text-neutral-400">
-            Already have an account?
-            <a href="{{ route('login') }}" class="font-semibold hover:underline dark:text-neutral-200" wire:navigate>
-                Log In
-            </a>
-        </p>
     </div>
+
 </form>
--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -1,14 +1,25 @@
 <x-guest-layout>
     <section class="w-full h-screen p-16 flex flex-col justify-center items-center">
-        <main class="px-16 py-4 flex flex-col justify-center items-center bg-neutral-100 dark:bg-gray-800 rounded-2xl">
-            <nav>
+        <main class="px-16 py-4 gap-4 flex flex-col justify-center items-center bg-neutral-100 dark:bg-gray-800 rounded-2xl">
+            <nav class="self-start">
                 <a href="{{ route('homepage') }}" wire:navigate>
                     <x-application-logo class="" />
                 </a>
             </nav>
-            <header>
+            <header class="pb-4 text-center">
+                <h3 class="text-black dark:text-neutral-200 text-4xl font-bold">Create your Account</h3>
+                <p class="text-gray-500 dark:text-neutral-400">This is where your posting journey begins.</p>
             </header>
             <livewire:pages.auth.register />
+            <footer class="">
+                <p class="text-center text-sm text-gray-600 dark:text-neutral-400">
+                    Already have an account?
+                    <a href="{{ route('login') }}" class="font-semibold hover:underline dark:text-neutral-200"
+                        wire:navigate>
+                        Log In
+                    </a>
+                </p>
+            </footer>
         </main>
     </section>
 </x-guest-layout>
